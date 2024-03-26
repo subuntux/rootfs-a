c1233959a23500c5e7e4ee708d74cf9ba0f64e54
subuntux <subuntu314@gmail.com>
2024-03-26 13:48:04 +0100

    create

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+unset LD_PRELOAD
+
+proot \
+  -0 \
+  --link2symlink \
+  -r ~/rootfs \
+  -b /dev/ \
+  -b /sys/ \
+  -b /proc/ \
+  -b /data/data/com.termux/files/home \
+  /usr/bin/env \
+    -i \
+    HOME=/root \
+    TERM="xterm-256color" \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+    /bin/bash --login
